d416533c0d5e4843226010d22f6295af096ffb8f
hbandi93 <hbandi93@gmail.com>
2021-10-21 12:00:23 +0200

    Stress cancelled. Mentoring session project fixed. Realizing landlock code doesn't work properly.

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 async function _load() {
     const rootElement = document.getElementById("root");
-    const res = await fetch("https://restcountries.com/v3.1/name/peru");
-    const peru = await res.json();
 
-    const peruO = peru[0];
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const countries = await res.json();
 
-    let landlockText = "";
+    console.log(countries);
 
-
-    if (peruO.landlocked = true) {
-        landlockText = "I've never met the sea.";
-    }
-    else {
-        landlockText = "I can go fishing for sea creatures here.";
-    }
-
-
-
-    const peruHTML = `
+    for (const country of countries) {
+        const countryHTML = `
         <section class="independent">
-            <img src="${peruO.flags.png}" alt="flag">
-            <h1><b>Official name:</b> ${peruO.name.official}</h1>
-            <h2><b>Capital:</b> ${peruO.capital[0]}</h2>
-            <ul>Languages:
-                <li>${peruO.languages.aym}</li>
-                <li>${peruO.languages.que}</li>
-                <li>${peruO.languages.spa}</li>
-            </ul>
-            <h5 id="landlock">${landlockText}</h5>
+            <img src="${country.flags.png}" alt="flag">
+            <h1><b>Official name:</b> ${country.name.official}</h1>
+            <h2><b>Capital:</b> ${country.capital}</h2>
             <!--<h5> Ha landlocked = true I've never met the sea.</h5>-->
         </section>
-    `;
+        `;
 
-
-
-    console.log(rootElement);
-
-    rootElement.insertAdjacentHTML("beforeend", peruHTML);
+        rootElement.insertAdjacentHTML("beforeend", countryHTML);
+    }
+    /*
+        let landlockText = "";
+    
+    
+        if (countries.landlocked = true) {
+            landlockText = "I've never met the sea.";
+        }
+        else {
+            landlockText = "I can go fishing for sea creatures here.";
+        }
+    
+    
+    
+    
+    
+    
+        console.log(rootElement);
+    
+        */
 
 }
 window.addEventListener("load", _load);
